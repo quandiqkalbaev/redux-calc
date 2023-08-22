da39f4e2f71f8269b0998d73e65cb37f49d47b47
Quandiq Kalbaev <132442061+quandiqkalbaev@users.noreply.github.com>
2023-08-22 17:04:48 +0500

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,7 @@ export default  function Header(){
         <div className="header">
             <div className="header__wrapper">
                 <Link className="header__link" to="/calculator">Calculator</Link>
-                <Link className="header__link" to="/task">Task</Link>
+
             </div>
         </div>
     )
